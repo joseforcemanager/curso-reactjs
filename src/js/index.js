627741776fdf7dfac8762f620e5f716f935ad90c
JOSE MARIA COMPANY GARCIA <companygarcia@gmail.com>
2017-05-01 21:10:15 +0200

    Context init

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import PropTypes  from 'prop-types';
 // Load SCSS
 import '../scss/app.scss';
 
-class Main extends React.Component {
+class Button extends React.Component {
+  
+  static propTypes = {
+    color: PropTypes.string.isRequired,
+  }
+
   render() {
     return (
-      <div className="main">
-        <h1>Hola mundo</h1>
+      <button style={{background: this.props.color}}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
+
+class Message extends React.Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.text} <Button color={this.props.color}>Delete</Button>
       </div>
     );
   }
 }
 
+class MessageList extends React.Component {
+  static propTypes = {
+    messages: PropTypes.array.isRequired,
+  }
+
+  render() {
+    const color = "red";
+    const children = this.props.messages.map((message) =>
+      <Message text={message.text} color={color} />
+    );
+    return <div>{children}</div>;
+  }
+}
+
+const messages = [{text:"uno"},{text:"dos"},{text:"tres"},{text:"cuatro"},{text:"cinco"},{text:"seis"}];
+
 // Render it to DOM
 ReactDOM.render(
-  <Main/>,
+  <MessageList messages={messages}/>,
   document.getElementById('root')
 );
