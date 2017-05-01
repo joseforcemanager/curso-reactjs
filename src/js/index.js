aef1b33270375cca4dd148b0f107974573351d57
JOSE MARIA COMPANY GARCIA <companygarcia@gmail.com>
2017-05-01 21:35:26 +0200

    plugin pref immutable(Poner ejemplo)

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './containers/root';
 
+import Perf from 'react-addons-perf';
+
+const isProduction = process.env.NODE_ENV === 'production';
+if(!isProduction){
+  window.Perf = Perf;
+}
+
+
 // Render it to DOM
 ReactDOM.render(
   <Root/>,
