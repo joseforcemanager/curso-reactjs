814f0dfad77aba9b85a6fd1264bebd7ae237af5c
JOSE MARIA COMPANY GARCIA <companygarcia@gmail.com>
2017-05-01 20:04:32 +0200

    Jugar con los props del hijo desde el padre, para mostrar que el hijo es reusable y es el container el que le pasa como tiene que mostrarse. Mostrar el plugin de react.

--- src/js/containers/root/index.jsx
+++ src/js/containers/root/index.jsx
@@ -2,18 +2,39 @@ import React from 'react';
 import LoginDialog from '../../components/login-dialog';
 // Load SCSS
 import './index.scss';
+import 'es6-promise';
+
+
+const loginAsync = (name,password) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      var re = /\S+@\S+\.\S+/;
+      resolve(re.test(name));
+      
+    }, (Math.random() * 1000) + 1000); // 1-2 seconds delay
+  });
+}
+
 
 class Main extends React.Component {
-
-  login = (name,password)=>{
-    debugger
+  state = {
+    loginLoading:false,
+    loginError:false,
+    isLogin:false,
   }
 
+  login = (name,password)=>{
+    this.setState({loginLoading:true,loginError:false,isLogin:false});
+    loginAsync(name,password)
+      .then(success => this.setState({loginLoading:false,loginError:!success,isLogin:success}))
+      .catch(error => this.setState({loginLoading:false,loginError:true,isLogin:false}));
+  }
 
   render() {
+    const {loginLoading,loginError,isLogin } = this.state;
     return (
       <div className="main">
-        <LoginDialog loginAction = {this.login} /*loading={false} error={true}*//>
+        <LoginDialog loginAction = {this.login} loading={loginLoading} error={loginError}/>
       </div>
     );
   }
